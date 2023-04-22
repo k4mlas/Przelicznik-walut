fd67ae23a85a350ec5195bddc55f0bd2b4b3661f
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-22 13:46:36 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 const dataLeft = document.querySelector('.rate__body__left__dataOne');
+const moneyLeft = document.querySelector('#currency-one');
+const dataRight = document.querySelector('.rate__body__right__dataTwo');
+const moneyRight = document.querySelector('#currency-two');
 const btn = document.querySelector('.rate__body__swap');
