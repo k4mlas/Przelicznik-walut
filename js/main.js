a713e6a317f3d562183cf46688f758446b874b00
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-22 14:00:16 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ const moneyLeft = document.querySelector('#currency-one');
 const dataRight = document.querySelector('.rate__body__right__dataTwo');
 const moneyRight = document.querySelector('#currency-two');
 const btn = document.querySelector('.rate__body__swap');
+const info = document.querySelector('.rate__info');
+
+const calculate = () => {
+	const URL = 'https://api.exchangerate.host/latest';
+	fetch(URL)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+};
+
+calculate();
